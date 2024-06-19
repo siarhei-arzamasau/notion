41d40ca06cdcec9bc1ef9fdb91521525191bada7
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-06-19 19:32:35 +0300

    use Page component in applicaiton

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import "./App.css";
 
-import { Cover } from './Page/Cover';
+import { Page } from "./Page/Page";
 
 function App() {
-
   return (
     <>
-      <Cover/>
+      <Page />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
