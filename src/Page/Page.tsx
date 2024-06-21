7eb87dc91e6be8f0fa136da42536b5bafc0c603c
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-06-21 22:46:42 +0300

    make nodes dragable

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -2,30 +2,48 @@ import { nanoid } from "nanoid";
 import { useFocusedNodeIndex } from "../hooks/useFocusedNodeIndex";
 import { Cover } from "./Cover";
 import { Spacer } from "./Spacer";
-import { NodeTypeSwitcher } from "../Node/NodeTypeSwitcher";
 import { Title } from "./Title";
 import { useAppState } from "../state/AppStateContext";
+import { DndContext, DragOverlay, DragEndEvent } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 import styles from "./Page.module.css";
+import { NodeContainer } from "../Node/NodeContainer";
 
 export const Page = () => {
-  const { title, nodes, addNode, setTitle } = useAppState();
+  const { title, nodes, addNode, setTitle, reorderNodes } = useAppState();
   const [focusedNodeIndex, setFocusedNodeInex] = useFocusedNodeIndex({ nodes });
+
+  const handleDragEvent = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (over?.id && active.id !== over?.id) {
+      reorderNodes(active.id as string, over.id as string);
+    }
+  };
 
   return (
     <>
       <Cover />
       <div>
         <Title addNode={addNode} title={title} changePageTitle={setTitle} />
-        {nodes.map((node, index) => (
-          <NodeTypeSwitcher
-            key={index}
-            node={node}
-            isFocused={focusedNodeIndex === index}
-            updateFocusedIndex={setFocusedNodeInex}
-            index={index}
-          />
-        ))}
+        <DndContext onDragEnd={handleDragEvent}>
+          <SortableContext items={nodes} strategy={verticalListSortingStrategy}>
+            {nodes.map((node, index) => (
+              <NodeContainer
+                key={index}
+                node={node}
+                isFocused={focusedNodeIndex === index}
+                updateFocusedIndex={setFocusedNodeInex}
+                index={index}
+              />
+            ))}
+          </SortableContext>
+          <DragOverlay />
+        </DndContext>
         <Spacer
           showHint={nodes.length === 0}
           handleClick={() => {
