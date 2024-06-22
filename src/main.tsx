18481f6560071d50ee7bea1451576d66e36d8fd6
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-06-22 22:05:33 +0300

    add auth context and hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import { AuthSessionProvider } from './auth/AuthSessionContext.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthSessionProvider>
+        <App />
+      </AuthSessionProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
